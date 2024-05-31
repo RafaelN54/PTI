97cd915d806a85e12015cf6779544602b375b5cc
RafaelN54 <rafael.natanael46@gmail.com>
2024-05-31 17:49:08 +0700

    deploy config

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -133,7 +133,7 @@ function Home() {
   return (
     <div className="kamar bg-cover bg-center h-screen w-screen relative flex flex-col justify-center">
       <StatusBarComponent name="statusBar" energy={energy} money={money} affinity={affinity} />
-      <div className="weather-box w-[150px] h-[180px] bg-blue-300 shadow-lg rounded-xl m-auto relative px-10 py-2 ">
+      <div className="weather-box w-[140px] h-[180px] bg-blue-300 shadow-lg rounded-xl m-auto relative px-10 py-2 ">
         <div className="flex justify-between w-full">
           <div className="w-full my-2 mx-auto flex flex-col items-start">
             <div>
